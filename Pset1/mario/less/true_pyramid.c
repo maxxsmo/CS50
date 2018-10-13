7bebe70c06732b30820e0bae3d3a0974dc0219c6
MpAiO <mpsmolis@gmail.com>
2018-10-13 16:11:24 +0200

    true pyramid added

---
+++ Pset1/mario/less/true_pyramid.c
@@ -0,0 +1,41 @@
+/*
+- Maxime Smolis
+- personal experiment -> make a true symetrical pyramid :
+- pattern : 
+
+               #
+              ###  
+             #####
+            ####### 
+           #########
+          ###########
+- hint : third for loop -> condition = hashes <= rows
+
+*/
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("number : ");
+    }
+    while (height <= 0 && height >= 23);
+    // set the pyramid
+    for(int rows=0; rows< height; rows++)
+    {
+        for(int spaces=(height - rows); spaces >0; spaces--)
+        {
+            printf(" ");
+        }
+
+        for(int hashes=0; hashes<= rows; hashes++)
+        {
+            printf("# ");
+        }
+            printf("\n");
+        }
+    }
